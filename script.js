fd607691696df240fda7c21139edc83535f5d492
newbasgaming <109394227+newbasgaming@users.noreply.github.com>
2022-07-16 19:06:37 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const target = document.querySelectorAll('[data-anime]')
+const animationClass = 'animate'
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4)
+    target.forEach(function(element){
+    if((windowTop) > element.offsetTop) {
+        element.classList.add('animate')
+    } else {
+        element.classList.remove(animationClass)
+    }
+        
+    })
+}
+
+animeScroll()
+
+if(target.length) {
+    window.addEventListener('scroll', function() {
+        animeScroll()
+    })
+}
+
